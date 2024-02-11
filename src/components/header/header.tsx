5d7fc78ed25ee16c497b32f1601dcc3b2c0a3d03
Bob Moriasi <107801004+bobbrysonn@users.noreply.github.com>
2024-02-10 19:53:17 -0500

    Finished working on the navigation bar

---
+++ src/components/header/header.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+import Navigation from "./navigation/navigation";
+import styles from "./header.module.css";
+
+export default function Header() {
+  return (
+    <header
+      className={`border-b-[1px] border-b-bg-dark gap-6 grid grid-cols-2 justify-between outline outline-1 outline-bg-light pb-4 pt-7 px-3 ${styles.header}`}
+    >
+      <Link
+        className="gap-6 grid grid-cols-[auto_1fr] hover:text-text text-white"
+        href={"/"}
+      >
+        <Image
+          width={64}
+          height={64}
+          className={"block rounded-full min-w-16"}
+          src={"/profile_pic.jpg"}
+          alt="Profile picture"
+          priority={true}
+        />
+        <div className="flex flex-col justify-center">
+          <p className="text-xl sm:text-xl md:text:2xl">Bob Moriasi</p>
+          <p className="duration-500 font-medium hover:text-text-light text-text-dark text-xs sm:text-sm transition-all">
+            Designer, Saxophonist, Student
+          </p>
+        </div>
+      </Link>
+
+      <Navigation />
+    </header>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "bg-dark": "#1C2F25",
+        "bg-light": "#2A4236",
+        highlight: "#E6A024",
+        text: "#FEFAE0",
+        "text-dark": "#97A97C",
+        "text-light": "#CCD5AE",
+      },
     },
   },
   plugins: [],
