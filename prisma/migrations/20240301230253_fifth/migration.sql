1d4815932bdf3b80a77aea9bf6fd369edabb7bc9
Bob Moriasi <107801004+bobbrysonn@users.noreply.github.com>
2024-03-01 19:35:36 -0500

    New Models

---
+++ prisma/migrations/20240301230253_fifth/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[github_id]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `github_id` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `username` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "github_id" INTEGER NOT NULL,
+ADD COLUMN     "username" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_github_id_key" ON "User"("github_id");
